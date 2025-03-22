2965705fa22c14ccafb7608242c83911afcb1ea7
ArceusMaxis <octohands@gmail.com>
2025-03-23 00:20:44 +0530

    PHOSPHORUS is da name, climax, MM, decals, icon, exports pending

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,13 +1,26 @@
 extends Control
 
+@onready var banner: TextureRect = $Banner
 @onready var play: Button = $VBoxContainer/Play
 @onready var credits: Button = $VBoxContainer/Credits
 @onready var gicj: Button = $VBoxContainer/GICJ
 @onready var quit: Button = $VBoxContainer/Quit
+@onready var label: Label = $Banner/Label
+
+var vis : bool = false
+var _timer = 0.0
+var blink_speed = 0.3
 const SEWERS = preload("res://scenes/sewers.tscn")
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_packed(SEWERS)
+	banner.visible = true
+	vis = true
+
+func labelblink(delta):
+	_timer += delta
+	if _timer >= blink_speed:
+		_timer = 0.0
+		label.visible = !label.visible
 
 func _on_credits_pressed() -> void:
 	OS.shell_open("https://chaak-007.itch.io")
@@ -17,3 +30,9 @@ func _on_gicj_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _physics_process(delta: float) -> void:
+	labelblink(delta)
+	if vis:
+		if Input.is_anything_pressed():
+			get_tree().change_scene_to_packed(SEWERS)
